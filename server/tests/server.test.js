057fad5caa1dea358f41534ed46824e1d66ddd23
sajjadhadafi <af.hadfi@gmail.com>
2018-04-17 10:06:53 +0430

    add test for Delete

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -229,10 +229,28 @@ describe("POST/user/login",()=>{
           return done(err);
         }
 
-        User.findById(users[1]._id).then((user) => {
-          expect(user.tokens.length).toBe(0);
-          done();
-        }).catch((e) => done(e));
-      });
-  });
+      User.findById(users[1]._id).then((user) => {
+        expect(user.tokens.length).toBe(0);
+        done();
+      }).catch((e) => done(e));
+    })
+  })
+})
+
+describe('DELETE /users/me/token',()=>{
+  it("should remove auth token on logout",(done)=>{
+    request(app)
+    .delete('/users/me/token')
+    .set('x-auth',users[0].tokens[0].token)
+    .expect(200)
+    .end((err,res)=>{
+      if(err){
+        return done(err)
+      }
+      User.findById(users[0]._id).then((user)=>{
+        expect(user.tokens.length).toBe(0)
+        done()
+      }).catch((e)=>done(e))
+    })
+  })
 })
